d6362e41509920d8bcf033ac89050cf6e0b26bb5
Katie Kirsch <katie.kirsch@gmail.com>
2023-06-22 13:30:49 -0400

    fix: add print statements for troubleshooting

--- scholarly/publication_parser.py
+++ scholarly/publication_parser.py
@@ -82,7 +82,7 @@ class _SearchScholarIterator(object):
     def __next__(self):
         print(f"url {self._url}")
         print(f"pos {self._pos}")
-        print(f"rows {self._rows}")
+        print(f"rows {len(self._rows)}")
         if self._pos < len(self._rows):
             row = self._rows[self._pos]
             self._pos += 1
