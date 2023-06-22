2860c70de059638f6cb47a6c31f21050a6963cc9
Katie Kirsch <katie.kirsch@gmail.com>
2023-06-22 15:46:01 -0400

    fix: add print statements for troubleshooting

--- scholarly/publication_parser.py
+++ scholarly/publication_parser.py
@@ -1,7 +1,7 @@
 import re
 import bibtexparser
 import arrow
-from time
+import time
 from bibtexparser.bibdatabase import BibDatabase
 from .data_types import BibEntry, Mandate, Publication, PublicationSource
 
